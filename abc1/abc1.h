9d2169daed10587cabe274c24b0dbb7b5e6a299a
Leo <agskishor@gmail.com>
2022-09-07 16:33:53 +0530

    Initial Commit

---
+++ abc1/abc1.h
@@ -0,0 +1,18 @@
+//
+//  abc1.h
+//  abc1
+//
+//  Created by MAcBook on 07/09/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for abc1.
+FOUNDATION_EXPORT double abc1VersionNumber;
+
+//! Project version string for abc1.
+FOUNDATION_EXPORT const unsigned char abc1VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <abc1/PublicHeader.h>
+
+
